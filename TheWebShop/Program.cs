98476b67cab48a7881682f93eceae3c032a052fe
Robin <loeveet@gmail.com>
2022-12-20 15:20:01 +0100

    Add project files.

---
+++ TheWebShop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TheWebShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
